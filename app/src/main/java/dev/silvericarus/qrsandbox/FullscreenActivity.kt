cd55124f9f5e6a0dfc3fa6fd6ea2137cb0b9333b
silvericarus <albin2@yahoo.com>
2022-11-22 22:23:25 +0100

    Adds scanning of QR

--- app/src/main/java/dev/silvericarus/qrsandbox/FullscreenActivity.kt
+++ app/src/main/java/dev/silvericarus/qrsandbox/FullscreenActivity.kt
@@ -2,6 +2,10 @@ package dev.silvericarus.qrsandbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.SearchManager
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -11,6 +15,7 @@ import android.view.View
 import android.view.WindowInsets
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.google.zxing.integration.android.IntentIntegrator
 import dev.silvericarus.qrsandbox.databinding.ActivityFullscreenBinding
 
 /**
@@ -22,6 +27,31 @@ class FullscreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFullscreenBinding
     private lateinit var fullscreenContent: TextView
     private lateinit var fullscreenContentControls: LinearLayout
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                val result = IntentIntegrator.parseActivityResult(resultCode, data)
+                AlertDialog.Builder(this)
+                    .setMessage(getString(R.string.scan_feedback_dialog_message) + "${result.contents}?")
+                    .setPositiveButton("Yes")
+                    { dialogInterface, i ->
+                        val intent = Intent(Intent.ACTION_WEB_SEARCH)
+                        intent.putExtra(SearchManager.QUERY, result.contents)
+                        startActivity(intent)
+                    }
+                    .setNegativeButton("No")
+                    { dialogInterface, i ->
+
+                    }
+                    .create()
+                    .show()
+            }
+        }
+    }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,10 +65,17 @@ class FullscreenActivity : AppCompatActivity() {
 
         fullscreenContentControls = binding.fullscreenContent
 
-        //Redirect to the scanner activity
+        //Do the scan of the QR Code
         binding.btnScan.setOnTouchListener { view, motionEvent ->
-            Log.d(view.id.toString(), motionEvent.rawX.toString())
-            Log.d(view.id.toString(), motionEvent.rawY.toString())
+            val intentIntegrator = IntentIntegrator(this)
+            intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            intentIntegrator.initiateScan()
+
+            val intent = Intent("com.google.zxing.client.android.SCAN")
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE")
+            intent.putExtra("SAVE_HISTORY", true)
+            startActivityForResult(intent, 0)
+
             return@setOnTouchListener (true)
         }
 
